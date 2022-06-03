868f4ff08a1e4c97dc53f1f194ac18dfdd03f651
jwandroid <cjd9408er@naver.com>
2022-06-03 17:58:28 +0900

    learn variable , type

---
+++ variable/bin/variable.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+  dynamic name = "name1";
+  Object name2 = "name2";
+  String name3 = "name3";
+  var value = "value";
+
+  dynamic x;
+  x = "1";
+  x = "2";
+
+  dynamic y;
+  y = "3";
+  y = 4;
+
+  const my = "me";
+
+  my = "ji";
+
+  print(y);
+}
