1c3d71de3a5e0120a4e89fed47d63da735cbae3a
jwAndroid <cjd9408er@naver.com>
2022-06-04 20:16:57 +0900

    prog list

---
+++ learn_dart/bin/collection.dart
@@ -0,0 +1,15 @@
+void main() {
+  list1();
+}
+
+void list1() {
+  List<int> arr = [1, 2, 3, 4, 5];
+  List<String> strArr = ["a", "b", "c"];
+
+  var arr1 = [1, 2, 3, 4, 5];
+  dynamic arr2 = ["a", "b", "c"];
+
+  var lay = arr.map((e) => e + 1);
+
+  strArr.forEach((element) { })
+}
